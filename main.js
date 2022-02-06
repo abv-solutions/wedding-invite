0f9102c588107b025912b78cbea2cbd730d3192a
Andrei Suciu <andreisuciu2005@yahoo.com>
2022-02-06 19:43:59 +0200

    Countdown

---
+++ main.js
@@ -0,0 +1,24 @@
+const countdown = document.querySelector('.countdown')
+const launchDate = new Date('Jul 16, 2022, 11:20:00').getTime()
+const interval = setInterval(() => {
+	const now = new Date().getTime()
+	const distance = launchDate - now
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+	const hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	)
+	const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+	countdown.innerHTML = `
+    <div>${days}<span>Zile</span></div>
+    <div>${hours}<span>Ore</span></div>
+    <div>${mins}<span>Min</span></div>
+    <div>${seconds}<span>Sec</span></div>
+  `
+	if (distance < 0) {
+		clearInterval(interval)
+		countdown.style.color = '#399c11'
+		countdown.style.fontWeight = 'bold'
+		countdown.innerHTML = 'Launched!'
+	}
+}, 1000)
