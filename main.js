4eb4064e13b6a3e242147f40b807bddfccd382e7
Andrei Suciu <andreisuciu2005@yahoo.com>
2022-02-12 17:14:01 +0200

    Added maps integration

--- main.js
+++ main.js
@@ -36,3 +36,54 @@ function changeResponse() {
 		console.log(refuseButton, hiddenInput, nameInput)
 	}
 }
+
+function initMap() {
+	const parc = { lat: 46.7686686, lng: 23.5774453 }
+	const wonderlandP = { lat: 46.720763, lng: 23.590652 }
+	const wonderlandS = { lat: 46.719931, lng: 23.589226 }
+
+	const map1 = new google.maps.Map(document.getElementById('map1'), {
+		zoom: 15,
+		center: parc,
+	})
+	const map2 = new google.maps.Map(document.getElementById('map2'), {
+		zoom: 16,
+		center: wonderlandP,
+	})
+	const map3 = new google.maps.Map(document.getElementById('map3'), {
+		zoom: 14,
+		center: wonderlandS,
+	})
+
+	const parkMarker = new google.maps.Marker({
+		position: parc,
+		map: map1,
+		title: 'Parcul Central',
+	})
+	const wonderlandPMarker = new google.maps.Marker({
+		position: wonderlandP,
+		map: map2,
+		title: 'Ponton Wonderland',
+	})
+	const wonderlandSMarker = new google.maps.Marker({
+		position: wonderlandS,
+		map: map3,
+		title: 'Orhideea Wonderland',
+	})
+
+	google.maps.event.addListener(parkMarker, 'click', function () {
+		window.open('https://goo.gl/maps/FSzCg8rEChMJqFJL8', '_blank')
+	})
+	google.maps.event.addListener(wonderlandPMarker, 'click', function () {
+		window.open(
+			"https://www.google.com/maps/place/46%C2%B043'14.8%22N+23%C2%B035'26.4%22E",
+			'_blank'
+		)
+	})
+	google.maps.event.addListener(wonderlandSMarker, 'click', function () {
+		window.open(
+			"https://www.google.com/maps/place/46%C2%B043'11.8%22N+23%C2%B035'21.2%22E/",
+			'_blank'
+		)
+	})
+}
